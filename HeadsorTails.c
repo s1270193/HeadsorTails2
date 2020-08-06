2f60f13d10c4abf16377221287e32361e64854ee
s1270193 <s1270193@gmail.com>
2020-08-06 20:37:46 +0900

    Final revision

--- HeadsorTails.c
+++ HeadsorTails.c
@@ -30,17 +30,18 @@ int main(){
         }
     }
 
-      printf("Heads: %d, Tails: %d\n", heads, tails);
+    printf("Heads: %d, Tails: %d\n", heads, tails);
 
     if(heads > tails)
     {
-        printf("You won!\n",);
+        printf("%s won!\n", name);
     }
     
     else
     {
-        printf("You lost!\n");
+        printf("%s lost!\n", name);
     }
 
+    
     return 0;
 }
