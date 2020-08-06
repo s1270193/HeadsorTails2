4dcc56cb0233bb99e54aa89794331c88e68f9356
s1270193 <s1270193@gmail.com>
2020-08-06 20:34:09 +0900

    Revert "Victory message added"
    This reverts commit 2db384b855e1abf08a630662192d5e9f29c624ec.

--- HeadsorTails.c
+++ HeadsorTails.c
@@ -29,18 +29,6 @@ int main(){
             printf("Round %d: Tails\n", i + 1);
         }
     }
-printf("Heads: %d, Tails: %d\n", heads, tails);
-
-    if(heads > tails)
-    {
-        printf("%s won!\n", name);
-    }
-    
-    else
-    {
-        printf("%s lost!\n", name);
-    }
-
 
   
     return 0;
